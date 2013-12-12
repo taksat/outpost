943bfc7ff18404179df2b3f9a0802599b22ac10b
taksat <taksat.aka@gmail.com>
2013-12-13 01:20:30 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Outpost::Application.config.secret_key_base = 'dfa84c8d87e0e5ec59ef1300cd33c1dc0d55604ca92a211ff4556b3614fa8fe62d613c4fabfedd862d457783eb4abeeb95c28a2a92ffc2180aa76253d6870988'
